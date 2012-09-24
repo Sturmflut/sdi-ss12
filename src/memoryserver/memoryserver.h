d586cf750e71db2d3655c64a734470c2cde90ac1
Thomas <Kanthak.Thomas@gmail.com>
2012-09-24 22:07:00 +0200

    implementation of startup, map_anon_pages, pagefault, startup - code has not been tested

--- src/memoryserver/memoryserver.h
+++ src/memoryserver/memoryserver.h
@@ -1,6 +1,11 @@
 #ifndef _MEMORYSERVER_H_
 #define _MEMORYSERVER_H_
 
+#include <sdi/sdi.h>
+
+#define NUM_T_ENTRY 30
+#define NOT_YET_MAPPED -1
+
 /*Data structures for memory server */
 
 typedef struct{
@@ -22,10 +27,24 @@ typedef struct{
       L4_ThreadId_t threadid;
       Page_entry_t pages[1000];
       File_entry_t filemaps[10];
+	unsigned int pages_index;
+	unsigned int filemaps_index;	
 } Taskheader_t;
 
+extern Taskheader_t taskList[NUM_T_ENTRY];
+
+extern unsigned char Taskheader_index;
 
 extern L4_ThreadId_t loggerid;
+extern L4_ThreadId_t sigma0id;
+extern CORBA_Environment env;
+extern char logbuf[80];
+
+extern void memoryserver_map_anon_pages_real(CORBA_Object  _caller, const L4_ThreadId_t * threadid, const L4_Word_t  type, const L4_Word_t  virt_start_address, const L4_Word_t  size, idl4_server_environment * _env);
+
+extern void  memoryserver_pagefault_real(CORBA_Object  _caller, const L4_Word_t  address, const L4_Word_t  ip, const L4_Word_t  privileges, idl4_fpage_t * page, idl4_server_environment * _env);
+
+extern void memoryserver_startup_real(CORBA_Object  _caller, const L4_ThreadId_t * threadid, const L4_Word_t  ip, const L4_Word_t  sp, idl4_server_environment * _env);
 
 void  memoryserver_server(void);
 
