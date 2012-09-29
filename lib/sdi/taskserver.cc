590d6c007dda6f12f38c51dc4de910db4cfc3151
Johannes Bittner <johannes.bittner@gmail.com>
2012-09-29 16:24:06 +0200

    Add taskserver.cc, update Makefile.in

---
+++ lib/sdi/taskserver.cc
@@ -0,0 +1,26 @@
+#include <sdi/sdi.h>
+#include <idl4glue.h>
+
+#include <sdi/constants.h>
+#include <stdlib.h>
+
+// We split the ThreadNo into a "task id" and a "thread count", so
+// when we are given a global thread id, we can deduce the task id from
+// it.
+
+L4_Word_t get_task_id(L4_ThreadId_t threadid) {
+    // get first 9 bit of thread no
+    return (L4_ThreadNo(threadid) >> 9);
+}
+
+
+L4_Word_t get_thread_count(L4_ThreadId_t threadid) {
+    // get last 9 bit of thread no
+    return (L4_ThreadNo(threadid) & (0xffffffff >> (32 - 9))); 
+}
+
+L4_ThreadId_t create_thread_id(L4_Word_t task_id, L4_Word_t thread_count) {
+    return (L4_GlobalId(task_id << 9 | thread_count, 1));
+}
+
+
