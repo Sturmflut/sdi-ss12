2a8dbddda87c69832927d128b2d4e47951e4811e
Johannes Bittner <johannes.bittner@gmail.com>
2012-10-11 11:59:08 +0200

    Add atoi() function

--- include/stdlib.h
+++ include/stdlib.h
@@ -192,5 +192,20 @@ extern inline word_t numBits( word_t bits )
     return ret;
     }
 
+// from dietlibc
+extern inline int atoi(const char* s) {
+    long int v=0;
+    int sign=1;
+    while ( *s == ' '  ||  (unsigned int)(*s - 9) < 5u) s++;
+    switch (*s) {
+        case '-': sign=-1;
+        case '+': ++s;
+    }
+    while ((unsigned int) (*s - '0') < 10u) {
+        v=v*10+*s-'0'; ++s;
+    }
+    return sign==-1?-v:v;
+}
+
 #endif
 
