8a7b46b0f079dbcadd255e957e0aa6a34a65a1e1
Simon Raffeiner <raffeiner@kit.edu>
2012-10-14 19:40:32 +0200

    pong: Implement a simple pong game

--- src/pong/main.cc
+++ src/pong/main.cc
@@ -14,119 +14,168 @@
 #include <if/iflogging.h>
 #include <if/ifnaming.h>
 #include <if/ifconsoleserver.h>
-#include <if/iffileserver.h>
-#include <if/iftaskserver.h>
 
 #include <sdi/console_attributes.h>
 #include <sdi/io.h>
 
+static int PADDLE_HEIGHT = 6;
+
 
 L4_ThreadId_t consoleid = L4_nilthread;
 L4_ThreadId_t loggerid = L4_nilthread;
-L4_ThreadId_t taskserverid = L4_nilthread;
+
 
 CORBA_Environment env(idl4_default_environment);
 
+
+void print_charat(int x, int y, char character, char attrib)
+{
+    if(x >= 0 && x < 80 && y >= 0 && y < 25)
+        IF_CONSOLESERVER_putcharat((CORBA_Object)consoleid, x, y, character, attrib, &env);
+}
+
+
+void print_stringat(int x, int y, char* text, char attrib)
+{
+    IF_CONSOLESERVER_putstringat((CORBA_Object)consoleid, x, y, text, attrib, &env);
+}
+
+
+void blank_line(int y)
+{
+    print_stringat(0, y, (char*)"                                                                                ", SDI_CONSOLE_ATTRIBUTE_FGBLACK | SDI_CONSOLE_ATTRIBUTE_BGBLACK);
+}
+
+
+void clear_screen()
+{
+    for(int y = 0; y < 25; y++)
+        blank_line(y);
+}
+
+
+void game_loop()
+{
+    // Dot parameters
+    char dotx = 3;
+    char doty = 12;
+    bool dotgoesright = true;
+    bool dotgoesdown = true;
+
+    // Paddle parameters
+    char paddley = 7;
+
+    // Result
+    int lostballs = 0;
+
+    char key;
+    char modifier;
+    char ret;
+    char strbuf[30];
+
+    while(1)
+    {
+        // Handle paddle
+        ret = IF_CONSOLESERVER_getKey((CORBA_Object)consoleid, &key, &modifier, &env);
+
+        log_printf(loggerid, "Char: '%c'", key);
+
+        if(ret && modifier == 0)
+        {
+            if(key == 'w')
+                paddley = paddley - 2;
+
+            if(key == 's')
+                paddley = paddley + 2;
+
+            if(paddley < 2)
+                paddley = 2;
+
+            if(paddley > (22 - PADDLE_HEIGHT))
+                paddley = 22 - PADDLE_HEIGHT;
+        }
+
+
+	// Handle vertical walls
+        if(doty == 1 && !dotgoesdown)
+            dotgoesdown = true;
+
+	if(doty == 24 && dotgoesdown)
+            dotgoesdown = false;
+
+        // Handle right wall
+        if(dotx >= 78 && dotgoesright)
+            dotgoesright = false;
+
+        // Handle paddle
+        if(dotx == 3 && !dotgoesright)
+        {
+            // Paddle catches ball
+            if(doty >= paddley && doty < (paddley + PADDLE_HEIGHT))
+                dotgoesright = true;
+            else
+           {
+               // Paddle does not catch ball
+               lostballs++;
+               dotx = 9;
+               dotgoesright = true;
+
+               print_stringat(36, 12, (char*)"BALL LOST", SDI_CONSOLE_ATTRIBUTE_FGLIGHTRED);
+               sleep(2000);
+               print_stringat(36, 12, (char*)"         ", 0);
+           }             
+        }
+
+        // Advance dot
+        if(dotgoesright)
+            dotx = dotx + 2;
+        else
+            dotx = dotx - 2;
+
+        if(dotgoesdown)
+            doty++;
+        else
+            doty--;
+
+
+
+        // Draw dot
+        print_charat(dotx, doty, '*', SDI_CONSOLE_ATTRIBUTE_FGLIGHTGREEN);
+
+        // Draw paddle
+        for(int i = 0; i < PADDLE_HEIGHT; i++)
+            print_charat(2, paddley + i, '|', SDI_CONSOLE_ATTRIBUTE_FGLIGHTWHITE);
+
+        // Draw result
+        snprintf(strbuf, sizeof(strbuf), "Balls lost: %i", lostballs);
+	print_stringat(0, 0, strbuf, SDI_CONSOLE_ATTRIBUTE_FGLIGHTYELLOW);
+   
+        sleep(150);
+
+
+        // Clear dot
+        print_charat(dotx, doty, ' ', 0);
+
+        // Clear paddle
+        for(int i = 0; i < PADDLE_HEIGHT; i++)
+            print_charat(2, paddley + i, ' ', 0);
+    }
+}
+
+
 int main()
 {
-	char logbuf[80];
+    sleep(300);
+    clear_screen();
 
-	int scancode;
-	int scanstatus;
+    // Resolve logger
+    while (L4_IsNilThread(loggerid))
+        loggerid = nameserver_lookup((path_t)"/server/logger");
 
-	while (L4_IsNilThread(loggerid))
-		loggerid = nameserver_lookup("/server/logger");
+    // Resolve console
+    while (L4_IsNilThread(consoleid))
+        consoleid = nameserver_lookup((path_t)"/server/console");
 
-    while (L4_IsNilThread(taskserverid)) {
-        taskserverid = nameserver_lookup("/task");
-    }
-    
-	IF_LOGGING_LogMessage((CORBA_Object)loggerid, "[SIMPLETHREAD2] Active", &env);
-    
-	char tbuf[10];
-	buf_t buf;
-	buf._buffer = (CORBA_char*)&tbuf;
-	buf._maximum = 10;	
-
-	/* Resolve fileserver */
-	L4_ThreadId_t fileid;
-	while (L4_IsNilThread(fileid))
-		fileid = nameserver_lookup("/file");
-
-
-	char logbuff[100];
-
-	/* testing get_file_id */
-	char *path = "/nameserver";
-	L4_Word_t res = IF_FILESERVER_get_file_id(fileid, path, &env);
-	snprintf(logbuff, sizeof(logbuff), "[TEST FILESERVER get_file_id for=%s] >>%d<< (expected '0')\n", path, res);
-	IF_LOGGING_LogMessage((CORBA_Object)loggerid, logbuff, &env);
-
-	char *path2 = "/bannerdemo";
-	res = IF_FILESERVER_get_file_id(fileid, path2, &env);
-	snprintf(logbuff, sizeof(logbuff), "[TEST FILESERVER get_file_id for=%s] >>%d<< (expected 'gr 0')\n", path2, res);
-	IF_LOGGING_LogMessage((CORBA_Object)loggerid, logbuff, &env);
-
-	char *path3 = "simplethread2";
-	res = IF_FILESERVER_get_file_id(fileid, path3, &env);
-	snprintf(logbuff, sizeof(logbuff), "[TEST FILESERVER get_file_id for=%s] >>%d<< (expected 'gr 0')\n", path3, res);
-	IF_LOGGING_LogMessage((CORBA_Object)loggerid, logbuff, &env);
-
-	char *path4 = "simplethread3";
-	res = IF_FILESERVER_get_file_id(fileid, path4, &env);
-	snprintf(logbuff, sizeof(logbuff), "[TEST FILESERVER get_file_id for=%s] >>%d<< (expected '-1')\n", path4, res);
-	IF_LOGGING_LogMessage((CORBA_Object)loggerid, logbuff, &env);
-
-
-	/* testing read	 */
-	const L4_Word_t  read_id = 2;
-	const L4_Word_t  offset = 0;
-	const L4_Word_t  count = 7;
-
-
-	buf_t buff;
-	char tbuff[50];
-	buff._buffer = (CORBA_char*)&tbuff;
-	buff._maximum = 50;
-	L4_Word_t res_read = IF_FILESERVER_read(fileid, read_id, offset, count, &buff, &env);
-
-	snprintf(logbuff, sizeof(logbuff), "[TEST FILESERVER read with fileid=%i] \"%s\" (Len %i)\n", read_id, buff._buffer, buff._length);
-	IF_LOGGING_LogMessage((CORBA_Object)loggerid, logbuff, &env);
-	//printf("%s --- result of read is %i\n", logbuff, res_read);
-
-	/* get_dir_size */
-	strcpy(logbuff, "");	//empty string
-	res = IF_FILE_get_dir_size(fileid, "/",&env);
-	snprintf(logbuff, sizeof(logbuff), "[TEST FILESERVER get_dir_size of '/'] = %d\n", res);
-	IF_LOGGING_LogMessage((CORBA_Object)loggerid, logbuff, &env);
-
-	/* get_file_size */
-	strcpy(logbuff, "");	//empty string
-	res = IF_FILE_get_file_size(fileid, "/consoleserver",&env);
-	snprintf(logbuff, sizeof(logbuff), "[TEST FILESERVER get_file_size of '/consoleserver'] = %lx\n", res);
-	IF_LOGGING_LogMessage((CORBA_Object)loggerid, logbuff, &env);
-
-	/* get_dir_entry */
-	buf_t dir_entry_buffer;
-	strncpy(tbuff, "", 50);
-	dir_entry_buffer._buffer = (CORBA_char*)&tbuff;
-	dir_entry_buffer._maximum = 50;
-
-	L4_Word_t result = IF_FILE_get_dir_entry(fileid, "/", 1, &dir_entry_buffer, &env);
-	snprintf(logbuff, sizeof(logbuff), "[TEST FILESERVER get_dir_entry nmb '1' of '/'] = '%s'\n (result: '%i')", dir_entry_buffer._buffer, result);
-	IF_LOGGING_LogMessage((CORBA_Object)loggerid, logbuff, &env);
-
-	buf_t dir_entry_buffer2;
-	strncpy(tbuff, "", 50);
-	dir_entry_buffer2._buffer = (CORBA_char*)&tbuff;
-	dir_entry_buffer2._maximum = 50;
-
-	result = IF_FILE_get_dir_entry(fileid, "/", 7, &dir_entry_buffer2, &env);
-	snprintf(logbuff, sizeof(logbuff), "[TEST FILESERVER get_dir_entry nmb '7' of '/'] = '%s'\n (result: '%i')", dir_entry_buffer2._buffer, result);
-	IF_LOGGING_LogMessage((CORBA_Object)loggerid, logbuff, &env);
-
-	/* Spin forever */
-	while (42) ;
-
-	return 0;
+    // Let's play!
+    game_loop();
 }
